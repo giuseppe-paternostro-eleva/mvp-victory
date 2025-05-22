e170981fb75d355a8e008195f0cb6e31257eeee7
giuseppe-paternostro-eleva <g.paternostro@eleva.it>
2025-05-22 14:37:22 +0200

    set env

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,7 +1,7 @@
 const API_URL =
   "/dev/forecast";
 
-const API_KEY = '';
+const API_KEY =  process.env.REACT_APP_API_KEY || '';
 
 export const baseInfo: { url: string; key: string } = {
   url: API_URL,
